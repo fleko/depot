5087715a97a6ecffa6a01cf6e5117994876a0d78
Ezequiel Aranda <earanda@gmail.com>
2020-09-04 17:52:31 -0300

    Task D

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,9 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+
+    session[:counter] = session[:counter].nil? ? 1 : session[:counter] + 1
+
+    @session_counter = session[:counter]
   end
 end
